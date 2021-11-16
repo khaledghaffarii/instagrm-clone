16e21c0c8a65e6d579bf0f522b90ac666ad69ee3
khaledghaffarii <med.khaledghaffary@gmail.com>
2021-11-16 16:50:55 +0100

    refactor: import and use a faker library and implimanted a child story component in Stories.js

--- components/Stories.js
+++ components/Stories.js
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
+import faker from "faker";
+import Story from "./Story";
+
 function Stories() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [suggestions, setSuggestions] = useState([]);
+  useEffect(() => {
+    const suggestions = [...Array(20)].map((_, i) => ({
+      ...faker.helpers.contextualCard(),
+      id: i,
+    }));
+    setSuggestions(suggestions);
+  }, []);
+  console.log(suggestions);
+  return (
+    <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black">
+      {Object.values(suggestions).map((profile) => {
+        return (
+          <Story key={profile.id} img={profile.avatar} username={profile.username} />
+        );
+      })}
+    </div>
+  );
 }
 
-export default Stories
+export default Stories;
