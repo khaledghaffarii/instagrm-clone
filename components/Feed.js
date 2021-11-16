d4ead75082b4cba49896bb39930b9d13bd63a09b
khaledghaffarii <med.khaledghaffary@gmail.com>
2021-11-16 16:47:11 +0100

    style: add style in feed component

--- components/Feed.js
+++ components/Feed.js
@@ -4,7 +4,7 @@ function Feed() {
   return (
     <main className="gird grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:max-w-3 xl:grid-cols-3 xl:max-w-6xl max-auto">
       {/* centerSection */}
-      <section>
+      <section className="col-span-2">
         {/* Stories */}
         <Stories/>
         {/* Posts */}
