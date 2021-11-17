74da9f5a6b874446cf7e7cf9196babae46f18d20
khaledghaffarii <med.khaledghaffary@gmail.com>
2021-11-17 09:19:51 +0100

    refactor: import the Posts component in components/Feed.js

--- components/Feed.js
+++ components/Feed.js
@@ -1,13 +1,15 @@
 import Stories from "./Stories";
+import Posts from './Posts';
 
 function Feed() {
   return (
-    <main className="gird grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:max-w-3 xl:grid-cols-3 xl:max-w-6xl max-auto">
+    <main className="gird grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:max-w-3 xl:grid-cols-3 xl:max-w-4xl mx-auto">
       {/* centerSection */}
       <section className="col-span-2">
         {/* Stories */}
         <Stories/>
         {/* Posts */}
+        <Posts/>
       </section>
 
       {/* rightSection */}
