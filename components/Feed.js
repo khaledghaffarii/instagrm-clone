80697404aeffe3254764e674715d5081e39b4529
khaledghaffarii <med.khaledghaffary@gmail.com>
2021-11-17 10:03:21 +0100

    refactor: import and implimated the two news components in components/Feed.js

--- components/Feed.js
+++ components/Feed.js
@@ -1,25 +1,21 @@
 import Stories from "./Stories";
-import Posts from './Posts';
-import MiniProfile from './MiniProfile';
-import Suggestions from './Suggestions';
+import Posts from "./Posts";
+import MiniProfile from "./MiniProfile";
+import Suggestions from "./Suggestions";
 
 function Feed() {
   return (
-    <main className="gird grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:max-w-3 xl:grid-cols-3 xl:max-w-4xl mx-auto">
-      {/* centerSection */}
+    <main className="gird grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-4xl mx-auto">
       <section className="col-span-2">
-        {/* Stories */}
-        <Stories/>
-        {/* Posts */}
-        <Posts/>
+        <Stories />
+        <Posts />
       </section>
 
-      {/* rightSection */}
-      <section>
-        {/* miniProfile */}
-        <MiniProfile/>
-        {/* suggestions */}
-        <Suggestions/>
+      <section className=" hidden xl:inline-grid md:col-span-1 float-right">
+        <div className="fixed top-20">
+          <MiniProfile />
+          <Suggestions />
+        </div>
       </section>
     </main>
   );
